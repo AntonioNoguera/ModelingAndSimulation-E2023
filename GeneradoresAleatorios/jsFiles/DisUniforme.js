2dec4e9b2e79a69dd54a0af2848cf9311b89fbf6
KarelyVR <karely_valdes07@live.com>
2023-03-03 12:22:40 -0600

    dist uniforme y <p>

--- GeneradoresAleatorios/jsFiles/DisUniforme.js
+++ GeneradoresAleatorios/jsFiles/DisUniforme.js
@@ -1,14 +1,35 @@
+
 function prueba1() {
+    var N = document.getElementById('numal').value;
     var a = document.getElementById('num').value;
     var b = document.getElementById('num1').value;
-    var Vu;
+    var ArrayVu=[];
+    var ArrayR=[];
         
-    let NumRandom = Math.random().toFixed(2);
-    console.log(NumRandom);
-    
-    Vu = parseInt(a) + NumRandom*(parseInt(b-a));
-    Vu.toFixed(2);
-    console.log(Vu);
+    if(N>10){
+        alert("No agregues mas de 10 elementos al vector...");
+    }else if((a<=0) || (b<=0) ){
+        alert("No puedes ingresar valores negativos o saldrá erroneo el resultado");
+    }
 
-    document.getElementById("Resultado").innerHTML = Vu;
+
+    for(var i=0; i<=N; i++){
+        
+        var NumRandom = Math.random().toFixed(2);
+        ArrayR.push(NumRandom);
+        console.log(ArrayR);
+    }
+    
+    document.getElementById('arreglo').innerHTML = ArrayR;
+    
+    for(var j=0; j<=N; j++ ){
+        
+        var Vu;
+        Vu = parseInt(a) + ArrayR[j]*(parseInt(a-b));
+        Vu.toFixed(2);
+        ArrayVu.push(Vu);
+        console.log(ArrayVu);
+    }
+
+    document.getElementById("Resultado").innerHTML = ArrayVu;
 }
